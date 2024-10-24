8f06fd047d0d4a3a9354f95e5a5d1c06ff5ef285
daabir mubashir <darmubashir2@gmail.com>
2024-10-24 20:19:07 +0530

    minor edits and dark mode
    minor edits to some text fields and added support for dark theme

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,7 @@
 import { HomeIcon, File, UsersRound } from "lucide-react";
 import Link from "next/link";
 import { NavButton } from "@/components/NavButton";
+import { ModeToggle } from "@/components/ModeToggle";
 
 export function Header(){
     return (
@@ -10,13 +11,14 @@ export function Header(){
                     <NavButton href="/home" label="Home" icon={HomeIcon} />
                     <Link href="/home" className="flex justify-center items-center gap-2 ml-0" title="Home">
                         <h1 className="hidden sm:block text-xl font-bold m-0 mt-1">
-                           Comp Repair Shop
+                           Daabir's Software Services Shop
                         </h1>
                     </Link>
                 </div>
                 <div className="flex items-center">
                     <NavButton href="/tickets" label="Tickets" icon={File} />
                     <NavButton href="/customers" label="Customers" icon={UsersRound} />
+                    <ModeToggle />
                 </div>
             </div>
         </header>
