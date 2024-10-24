2a6da8cf4209ff8a769ee0425b75a8adc544575e
daabir mubashir <darmubashir2@gmail.com>
2024-10-24 23:19:35 +0530

    added auth with kinde

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,9 @@
-import { HomeIcon, File, UsersRound } from "lucide-react";
+import { HomeIcon, File, UsersRound, LogOut } from "lucide-react";
 import Link from "next/link";
 import { NavButton } from "@/components/NavButton";
 import { ModeToggle } from "@/components/ModeToggle";
+import { LogoutLink } from "@kinde-oss/kinde-auth-nextjs/components";
+import { Button } from "@/components/ui/button"; 
 
 export function Header(){
     return (
@@ -19,6 +21,18 @@ export function Header(){
                     <NavButton href="/tickets" label="Tickets" icon={File} />
                     <NavButton href="/customers" label="Customers" icon={UsersRound} />
                     <ModeToggle />
+                    <Button
+                        variant="ghost"
+                        size="icon"
+                        aria-label="LogOut"
+                        title="LogOut"
+                        className="rounded-full"
+                        asChild
+                    >
+                        <LogoutLink>
+                            <LogOut/>
+                        </LogoutLink>
+                    </Button>
                 </div>
             </div>
         </header>
