38abd68636a89797c9fcfc5f1185907ca6f895af
daabir mubashir <darmubashir2@gmail.com>
2024-12-15 21:50:59 +0530

    Update CustomerForm.tsx

--- src/app/(rs)/customers/form/CustomerForm.tsx
+++ src/app/(rs)/customers/form/CustomerForm.tsx
@@ -1,36 +1,36 @@
 "use client"
 
 import { useForm } from "react-hook-form"
-import { zodResolver} from "@hookform/resolvers/zod"
+import { zodResolver } from "@hookform/resolvers/zod"
 import { Form } from "@/components/ui/form"
 import { Button } from "@/components/ui/button"
-import { insertCustomerSchema, type insertCustomerSchemaType, type selectCustomerSchemaType } from "@/zod-schemas/customer"
 
-import { TextAreaWithLabel } from "@/components/inputs/TextAreaWithLabel"
-import { SelectWithLabel } from "@/components/inputs/SelectWithLabel"
-import { StatesArray } from "@/constants/StatesArray"
 import { InputWithLabel } from "@/components/inputs/InputWithLabel"
+import { SelectWithLabel } from "@/components/inputs/SelectWithLabel"
+import { TextAreaWithLabel } from "@/components/inputs/TextAreaWithLabel"
 import { CheckboxWithLabel } from "@/components/inputs/CheckboxWithLabel"
 
-import {useKindeBrowserClient} from "@kinde-oss/kinde-auth-nextjs"
+import { useKindeBrowserClient } from "@kinde-oss/kinde-auth-nextjs"
 
-import { useAction } from "next-safe-action/hooks"
+import { StatesArray } from "@/constants/StatesArray"
+
+import { insertCustomerSchema, type insertCustomerSchemaType, type selectCustomerSchemaType } from "@/zod-schemas/customer"
+
+import { useAction } from 'next-safe-action/hooks'
 import { saveCustomerAction } from "@/app/actions/saveCustomerAction"
-
-import { useToast } from "@/hooks/use-toast"
-import { LoaderCircle } from "lucide-react"
+import { useToast } from '@/hooks/use-toast'
+import { LoaderCircle } from 'lucide-react'
+import { DisplayServerActionResponse } from "@/components/DisplayServerActionResponse"
 
 type Props = {
-    customer?:selectCustomerSchemaType,
+    customer?: selectCustomerSchemaType,
 }
 
-export default function CustomerForm({customer}: Props) {
+export default function CustomerForm({ customer }: Props) {
     const { getPermission, isLoading } = useKindeBrowserClient()
     const isManager = !isLoading && getPermission('manager')?.isGranted
-    
-    const { toast } = useToast();
-    // const permObj = getPermissions()
-    // const isAuthorized = !isLoading && permObj.permissions.some(perm => perm === 'manager' || perm === 'admin')
+
+    const { toast } = useToast()
 
     const defaultValues: insertCustomerSchemaType = {
         id: customer?.id ?? 0,
@@ -44,134 +44,154 @@ export default function CustomerForm({customer}: Props) {
         phone: customer?.phone ?? '',
         email: customer?.email ?? '',
         notes: customer?.notes ?? '',
-        active: customer?.active ?? true,     
+        active: customer?.active ?? true,
     }
 
     const form = useForm<insertCustomerSchemaType>({
         mode: 'onBlur',
         resolver: zodResolver(insertCustomerSchema),
         defaultValues,
-
     })
 
     const {
         execute: executeSave,
         result: saveResult,
-        isExecuting: isSaving,
+        isPending: isSaving,
         reset: resetSaveAction,
     } = useAction(saveCustomerAction, {
         onSuccess({ data }) {
-            // Toast user
-            toast({
-                variant: "default",
-                title: "Success!",
-                description: data?.message,
-            })
+            if (data?.message) {
+                toast({
+                    variant: "default",
+                    title: "Success! 🎉",
+                    description: data.message,
+                })
+            }
         },
-        onError({ error }){
-            // Toast user
+        onError({ error }) {
             toast({
                 variant: "destructive",
-                title: "Error!",
-                description: "Save Failed!",
+                title: "Error",
+                description: "Save Failed",
             })
         }
     })
 
     async function submitForm(data: insertCustomerSchemaType) {
-        // console.log(data);
         executeSave(data)
-        
     }
-    return(
+
+    return (
         <div className="flex flex-col gap-1 sm:px-8">
+            <DisplayServerActionResponse result={saveResult} />
             <div>
                 <h2 className="text-2xl font-bold">
                     {customer?.id ? "Edit" : "New"} Customer {customer?.id ? `#${customer.id}` : "Form"}
                 </h2>
             </div>
             <Form {...form}>
-                <form 
+                <form
                     onSubmit={form.handleSubmit(submitForm)}
                     className="flex flex-col md:flex-row gap-4 md:gap-8"
                 >
                     <div className="flex flex-col gap-4 w-full max-w-xs">
+
                         <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="First Name"
                             nameInSchema="firstName"
                         />
+
+                        <InputWithLabel<insertCustomerSchemaType>
+                            fieldTitle="Last Name"
+                            nameInSchema="lastName"
+                        />
+
                         <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="Address 1"
                             nameInSchema="address1"
                         />
+
                         <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="Address 2"
                             nameInSchema="address2"
                         />
+
                         <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="City"
                             nameInSchema="city"
                         />
+
                         <SelectWithLabel<insertCustomerSchemaType>
                             fieldTitle="State"
                             nameInSchema="state"
                             data={StatesArray}
                         />
-                        
+
                     </div>
+
                     <div className="flex flex-col gap-4 w-full max-w-xs">
-                    <InputWithLabel<insertCustomerSchemaType>
+
+                        <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="Pin Code"
                             nameInSchema="pin"
-                    />
-                    <InputWithLabel<insertCustomerSchemaType>
+                        />
+
+                        <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="Email"
                             nameInSchema="email"
-                    />
-                    <InputWithLabel<insertCustomerSchemaType>
+                        />
+
+                        <InputWithLabel<insertCustomerSchemaType>
                             fieldTitle="Phone"
                             nameInSchema="phone"
-                    />
-                    <TextAreaWithLabel<insertCustomerSchemaType>
-                        fieldTitle="Notes"
-                        nameInSchema="notes"
-                        className="h-40"
-                    />
+                        />
 
-                    {isLoading? <p>Loading ...</p> : isManager && customer?.id ? (
-                    <CheckboxWithLabel<insertCustomerSchemaType>
-                        fieldTitle="Active"
-                        nameInSchema="active"
-                        message="Yes"
-                    />) : null }
+                        <TextAreaWithLabel<insertCustomerSchemaType>
+                            fieldTitle="Notes"
+                            nameInSchema="notes"
+                            className="h-40"
+                        />
 
-                    <div className="flex gap-2">
-                        <Button
-                            type="submit"
-                            className="w-3/4"
-                            variant="default"
-                            title="Save"
-                            disabled={isSaving}
-                        >
-                            {isSaving ? (
-                                <><LoaderCircle className="animate-spin" />Saving</>
-                            ): "Save"}
-                        </Button>
-                        <Button
-                            type="button"
-                            variant="destructive"
-                            title="Reset"
-                            onClick={()=>{
-                                form.reset(defaultValues)
-                                resetSaveAction()
-                            }}
-                        >
-                            Reset
-                        </Button>
+                        {isLoading ? <p>Loading...</p> : isManager && customer?.id ? (
+                            <CheckboxWithLabel<insertCustomerSchemaType>
+                                fieldTitle="Active"
+                                nameInSchema="active"
+                                message="Yes"
+                            />) : null}
+
+                        <div className="flex gap-2">
+                            <Button
+                                type="submit"
+                                className="w-3/4"
+                                variant="default"
+                                title="Save"
+                                disabled={isSaving}
+                            >
+                                {isSaving ? (
+                                    <>
+                                        <LoaderCircle className="animate-spin" /> Saving
+                                    </>
+                                ) : "Save"}
+                            </Button>
+
+                            <Button
+                                type="button"
+                                variant="destructive"
+                                title="Reset"
+                                onClick={() => {
+                                    form.reset(defaultValues)
+                                    resetSaveAction()
+                                }}
+                            >
+                                Reset
+                            </Button>
+                        </div>
+
                     </div>
-                    </div>
+
                 </form>
             </Form>
+
         </div>
     )
 }
