a5d8f4b15d8e81e2946a1d338c7a2e422906629c
daabir mubashir <darmubashir2@gmail.com>
2025-01-11 19:11:36 +0530

    Add search functionality

---
+++ src/lib/queries/getCustomerSearchResults.ts
@@ -0,0 +1,20 @@
+import { db } from "@/db"
+import { customers } from "@/db/schema"
+import { ilike, or } from "drizzle-orm"
+
+export async function getCustomerSearchResults(searchText:string) {
+    const results = await db.select()
+        .from(customers)
+        .where(or(
+            ilike(customers.firstName, `%${searchText}%`),
+            ilike(customers.lastName, `%${searchText}%`),
+            ilike(customers.email, `%${searchText}%`),
+            ilike(customers.address1, `%${searchText}%`),
+            ilike(customers.address2, `%${searchText}%`),
+            ilike(customers.city, `%${searchText}%`),
+            ilike(customers.state, `%${searchText}%`),
+            ilike(customers.pin, `%${searchText}%`),
+            ilike(customers.notes, `%${searchText}%`),
+        ))
+    return results
+}
