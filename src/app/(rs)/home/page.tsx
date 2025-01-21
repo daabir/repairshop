e62c88ac7f3dc4397bfbf67a55f012178b3654b9
daabir mubashir <darmubashir2@gmail.com>
2025-01-21 23:44:36 +0530

    code refactoring

--- src/app/(rs)/home/page.tsx
+++
@@ -1,9 +0,0 @@
-import { redirect } from "next/navigation"
-// export const metadata = {
-//     title: "Home",
-// }
-
-export default function Home(){
-    // return <h2>Home</h2>
-    redirect("/tickets")
-}
