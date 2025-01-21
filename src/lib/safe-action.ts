242ea7ccb05e24f540d057d0aa9f5521235ca468
daabir mubashir <darmubashir2@gmail.com>
2025-01-22 01:30:07 +0530

    Update safe-action.ts

--- src/lib/safe-action.ts
+++ src/lib/safe-action.ts
@@ -1,5 +1,6 @@
 import { createSafeActionClient, DEFAULT_SERVER_ERROR_MESSAGE } from "next-safe-action"
 import { z } from "zod"
+import type { NeonDbError } from "@neondatabase/serverless"
 
 export const actionClient = createSafeActionClient({
     defineMetadataSchema(){
@@ -9,7 +10,13 @@ export const actionClient = createSafeActionClient({
     },
     handleServerError(e){
         if (e.constructor.name === 'NeonDbError') {
-            return "Database Error!"
+            const { code, detail } = e as NeonDbError
+            if (code === "23505") {
+                return `Unique entry required! ${detail}`
+            }
+            else{
+                return "Database Error! Contact support if error persists!"
+            }
         }
         return e.message
     }
