189949aacdc5a06c37af81193c7c2bc844a31392
daabir mubashir <darmubashir2@gmail.com>
2025-01-22 16:59:11 +0530

    bug fixes

--- src/app/(rs)/customers/form/page.tsx
+++ src/app/(rs)/customers/form/page.tsx
@@ -37,10 +37,10 @@ export default async function CustomerFormPage({
             }
             
             // customer form component
-            return <CustomerForm isManager={isManager} customer={customer}/>
+            return <CustomerForm key={customerId} isManager={isManager} customer={customer}/>
         } else {
             // new customer
-            return <CustomerForm isManager={isManager} />
+            return <CustomerForm key="new" isManager={isManager} />
         }
     } catch (e) {
         if (e instanceof Error){
